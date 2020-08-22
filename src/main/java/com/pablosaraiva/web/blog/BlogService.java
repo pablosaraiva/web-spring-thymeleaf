84eb038e841365cfe7b285017f37246c02117f37
Pablo Saraiva <pablosaraiva@gmail.com>
2020-08-22 08:45:07 +0200

    Lists blog posts

--- src/main/java/com/pablosaraiva/web/blog/BlogService.java
+++ src/main/java/com/pablosaraiva/web/blog/BlogService.java
@@ -3,16 +3,25 @@ package com.pablosaraiva.web.blog;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class BlogService {
     public List<BlogPost> getLastPostsSummaries(int numberOfPosts) {
-        final BlogPost blogPost = new BlogPost(
-                "First post",
-                "This is the content",
-                "This is the summary", LocalDateTime.of(2020, 8, 22, 8, 18));
+        return generatePosts(5);
+    }
 
-        return List.of(blogPost);
+    private List<BlogPost> generatePosts(int numberOfPosts) {
+        final List<BlogPost> blogPosts = new ArrayList<>(numberOfPosts);
+        for (int i = 0; i < numberOfPosts; i++) {
+            blogPosts.add(new BlogPost(
+                    "Post " + UUID.randomUUID(),
+                    "This is the content",
+                    "This is the summary", LocalDateTime.of(2020, 8, 22, 8, 18)));
+        }
+
+        return blogPosts;
     }
 }
