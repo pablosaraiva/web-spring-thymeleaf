23f27781ec212fc2c38fdaabe307b36d98cf1ba8
Pablo Saraiva <pablosaraiva@gmail.com>
2020-08-24 18:46:06 +0200

    - Add formatting to the create blog form  - Add conditional secure menu on home

--- src/main/java/com/pablosaraiva/web/webcontrollers/IndexController.java
+++ src/main/java/com/pablosaraiva/web/webcontrollers/IndexController.java
@@ -1,13 +1,20 @@
 package com.pablosaraiva.web.webcontrollers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/index")
-    public String getIndex() {
+    public String getIndex(Principal principal, Model model) {
+        if (principal != null) {
+            model.addAttribute("principalName", principal.getName());
+        }
+
         return "index";
     }
 }
