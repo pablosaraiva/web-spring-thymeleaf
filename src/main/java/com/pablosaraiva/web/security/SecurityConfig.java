43b8d638f7bb7753994e3a2d3f9f2a14db0709fd
Pablo Saraiva <pablosaraiva@gmail.com>
2020-09-15 21:20:25 +0200

    Security fix  - Fix SecurityConfig matcher

--- src/main/java/com/pablosaraiva/web/security/SecurityConfig.java
+++ src/main/java/com/pablosaraiva/web/security/SecurityConfig.java
@@ -12,7 +12,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/secure/*").authenticated()
+                .antMatchers("/secure/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
                 .oauth2Login();
